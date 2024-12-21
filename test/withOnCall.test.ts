eb93d24ef83532f808ab25e16cf6a2b1ccde831e
Florian Patzke <florian.patzke@t-online.de>
2024-12-21 16:00:29 +0100

    Füge Tests für die Funktion withOnCall hinzu und aktualisiere die Logik in den Tests für withOnCatch, um das Verhalten bei fehlenden Event-Handlern und Fehlerbehandlung zu überprüfen.

--- test/withOnCall.test.ts
+++ test/withOnCall.test.ts
@@ -4,15 +4,15 @@ import { vi } from "vitest";
 
 describe("withOnCall", () => {
 
-  it("should return the original function if no event handler is provided", () => {
-    const fn = vi.fn();
-    const wrapped = withOnCall(fn);
-    expect(wrapped).toBe(fn);
-  });
-
   it("should return a function", () => {
     const onCall = withOnCall(() => {});
     expect(onCall).toBeInstanceOf(Function);
+  });
+
+  it("should return the original function if no event handler is provided", () => {
+    const fn = () => {};
+    const wrapped = withOnCall(fn);
+    expect(wrapped).toBe(fn);
   });
 
   it("should throw an error if the callee is not a function", () => {
@@ -21,9 +21,15 @@ describe("withOnCall", () => {
 
   it("should call the onCall event handler", () => {
     const onCall = vi.fn();
-    const wrapped = withOnCall(()=>{},onCall);
-    wrapped();
-    expect(onCall).toHaveBeenCalled();
+    const wrapped = withOnCall((p)=>p,onCall);
+    wrapped("test");
+    expect(onCall).toHaveBeenCalledWith(
+      {
+        callee: expect.any(Function),
+        args: ["test"],
+        event: "onCall"
+      }
+    );
   });
 
   it("should pass the arguments to the onCall event handler", () => {
@@ -35,12 +41,10 @@ describe("withOnCall", () => {
   });
 
   it("should short circuit the function with the result returned from the onCall event handler", () => {
-    const spy = vi.fn();
-    const onCall = (params) => { spy(params); return {result: "result"}};
+    const onCall = () => ({result: "result"});
     const originalFunction = vi.fn();
     const wrapped = withOnCall(originalFunction, onCall);
     const result = wrapped("test");
     expect(result).toBe("result");
-    expect(spy).toHaveBeenCalledWith({callee: originalFunction, args: ["test"], event: "onCall"});
   });
 });
--- test/withOnCatch.test.ts
+++ test/withOnCatch.test.ts
@@ -2,15 +2,16 @@ import { withOnCatch } from "../src/withOnCatch";
 import { vi } from "vitest";
 
 describe("withOnCatch", () => {
-  it("should return the original function if no event handler is provided", () => {
-    const fn = vi.fn();
-    const wrapped = withOnCatch(fn);
-    expect(wrapped).toBe(fn);
-  });
 
   it("should return a function", () => {
     const onCatch = withOnCatch(() => {});
     expect(onCatch).toBeInstanceOf(Function);
+  });
+
+  it("should return the original function if no event handler is provided", () => {
+    const fn = () => {};
+    const wrapped = withOnCatch(fn);
+    expect(wrapped).toBe(fn);
   });
 
   it("should throw an error if the callee is not a function", () => {
@@ -45,6 +46,16 @@ describe("withOnCatch", () => {
     const onCatch = () => ({ result: "caught" });
     const wrapped = withOnCatch(func, onCatch as any);
     const result = await wrapped();
+    expect(result).toBe("caught");
+  });
+
+  it("should supress an error and return a value for a sync function", () => {
+    const func = () => {
+      throw new Error("test");
+    };
+    const onCatch = () => ({ result: "caught" });
+    const wrapped = withOnCatch(func, onCatch as any);
+    const result = wrapped();
     expect(result).toBe("caught");
   });
 });
