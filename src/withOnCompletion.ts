c2f7d0e10abf25746875ba7448ca229f8c03d03b
Florian Patzke <florian.patzke@t-online.de>
2024-12-26 10:47:44 +0100

    Refaktoriere die Funktion withExecution zur Verbesserung der Callback-Logik und füge Unterstützung für benutzerdefinierte Ereignishandler hinzu.

--- src/withOnCompletion.ts
+++ src/withOnCompletion.ts
@@ -54,87 +54,38 @@ type CallbackEventOptions<F extends (...args: any) => any> = {
   caught?: unknown;
 };
 
-const _callback = (
-  parameters: CallbackEventOptions<any>,
-  callbacks: {
-    [name: string]: (
-      params: CallbackEventOptions<any>
-    ) => Partial<CallbackEventOptions<any>>;
-  }
-) => {
-  const { event, callee, args, result, caught } = parameters;
-  if (event === "onExecution") {
-    // this should handle the cases for onCall (short circuiting) and onExecution
-    // if on Call is present, it should be called with the arguments, and the result should be passed to
-    // onExecution short circuiting the execution of the function
-    // or it should pass the modified callee and/or args to the onExecution handler
-    return callbacks.onExecution?.({
-      event: "onReturn",
-      callee,
-      args,
-      result,
-      caught,
-      ...(callbacks.onCall
-        ? callbacks.onCall({ event: "onCall", callee, args })
-        : {}),
-    });
-  }
-  if (event === "onCatch") {
-    // this should handle the cases for onCatch
-    // if onCatch is present, it should be called with the caught value,
-    // so that the onCatch handler can suppress errors and return a value instead
-    // or if it is not present, it should return the caught value
-    return callbacks.onCatch
-      ? callbacks.onCatch({ event: "onCatch", callee, args, result, caught })
-      : { caught };
-  }
-  return { callee, args, result, caught };
-};
+const passThrough = (...args:any) => args;
 
 const withExecution = <F extends (...args: any) => any>(
   callee: F,
-  callback: (
-    params: Partial<CallbackEventOptions<F>>
-  ) => CallbackEventOptions<F>
+  handlers: { [handler:string] : (params:CallbackEventOptions<F>) => Partial<CallbackEventOptions<F>> } | undefined = {}
 ) => {
   throwIfNotCallable(callee);
-  return isSynchronous(callee)
-    ? (...args: Parameters<F>): ReturnType<F> | undefined => {
+  const wrapped = Object.keys(handlers).length === 0 ? (...args:Parameters<F>) => callee(...args) : (async (...args: Parameters<F>) => {
         try {
-          return callback({ event: "onExecution", callee, args }).result;
+          return await handlers.onExecution?.({ event: "onExecution", callee, args }).result || callee(...args);
         } catch (error) {
-          const { caught, result } = callback({
+          const { caught, result } = handlers.onCatch?.({
             event: "onCatch",
             callee,
             args,
             caught: error,
-          });
+          }) || { caught: error };
           if (caught) {
             throw caught;
           } else {
             return result;
           }
         }
-      }
-    : async (...args: Parameters<F>): Promise<ReturnType<F> | undefined> => {
-        return new Promise((resolve, reject) => {
-          try {
-            resolve(callback({ event: "onExecution", callee, args }).result);
-          } catch (error) {
-            const { caught, result } = callback({
-              event: "onCatch",
-              callee,
-              args,
-              caught: error,
-            });
-            if (caught) {
-              reject(caught);
-            } else {
-              resolve(result);
-            }
-          }
-        });
-      };
+      }) as F & { on?: (event: string, handler: (params: CallbackEventOptions<F>) => Partial<CallbackEventOptions<F>>) => F };
+  const trigger = (event: string, params: CallbackEventOptions<F>) => {};
+  wrapped.on = (event:string, handler: (params: CallbackEventOptions<F>,t:typeof trigger) => Partial<CallbackEventOptions<F>>) => {
+    return wrapped;
+  };
+  return wrapped;
 };
+
+
+const wrapped = withExecution(() => {})
 
 export { withOnCompletion };
