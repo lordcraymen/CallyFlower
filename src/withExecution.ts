d71fe7f58dffadf26d735edbe96cc756bef149c3
Florian Patzke <florian.patzke@t-online.de>
2024-12-28 15:45:05 +0100

    Verbessere die Funktion withExecution durch die Umstrukturierung der Handler-Parameter und verbessere die Fehlerbehandlung; aktualisiere die Logik für synchrone und asynchrone Rückrufe.

--- src/withExecution.ts
+++ src/withExecution.ts
@@ -1,58 +1,63 @@
 import { throwIfNotCallable, isSynchronous } from "./utils"
 
 type CallbackEventOptions<F extends (...args: any) => any> = {
-  event: string
   callee: F
   args: Parameters<F>
   result?: ReturnType<F>
   caught?: unknown
 }
 
-const _handleEvent = <F extends (...args: any) => any>({handler, event, callee, args, result} : CallbackEventOptions<F> & { handler?: (params: CallbackEventOptions<F>) => Partial<CallbackEventOptions<F>> | void | undefined }) => 
-  handler?.({ event, callee, args, result })?.result ?? (result || callee(...args))
+const _handleEvent = <F extends (...args: any) => any>(
+  { handler, callee, args, result }: CallbackEventOptions<F> & {
+    handler?: (
+      params: CallbackEventOptions<F>
+    ) => Partial<CallbackEventOptions<F>> | void | undefined;
+  }
+) => {
+  const handlerResult = handler?.({callee, args, result });
+  return handlerResult && typeof handlerResult === "object" && "result" in handlerResult
+    ? handlerResult.result
+    : result ?? callee(...args);
+};
+
+
 
 const _handleCatch = <F extends (...args: any) => any>(
-  onCatch: (params: CallbackEventOptions<F>) => Partial<CallbackEventOptions<F>> | void | undefined,
+  { handler, callee, args, caughtValue }:
+  {
+  handler?: (params: CallbackEventOptions<F>) => {result?: ReturnType<F>, caught?: unknown} | void,
   callee: F,
   args: Parameters<F>,
-  caughtValue: unknown
+  caughtValue: unknown}
 ) => {
-  const { caught, result } = onCatch?.({
-    event: "onCatch",
-    callee,
-    args,
-    caught: caughtValue,
-  }) || { caught: caughtValue }
+  const { caught, result } = handler?.({ callee, args, caught: caughtValue}) || { caught: caughtValue }
   if (caught) { throw caught}
   return result
 }
 
 const withExecution = <F extends (...args: any) => any>(
   c: F,
-  handlers:
-    | {
-        [handler: "onCall" | "onReturn" | "onCatch" | string]: (
-          params: CallbackEventOptions<F>
-        ) => Partial<CallbackEventOptions<F>> | void
-      }
-    | undefined = {}
+  { onCall, onReturn, onCatch }: {
+    onCall?: (params: CallbackEventOptions<F>) => Partial<CallbackEventOptions<F>> | void;
+    onReturn?: (params: CallbackEventOptions<F>) => Partial<CallbackEventOptions<F>> | void;
+    onCatch?: (params: CallbackEventOptions<F>) => {result?: ReturnType<F>, caught?: unknown} | void;
+  }
 ) => {
   const wrapped = isSynchronous(c)
     ? function (this: any, ...args: Parameters<F>) {
         const callee = ((...args: Parameters<F>) => c.apply(this, args)) as F
         try {
-          return _handleEvent({event: "onReturn", callee, args, handler: handlers.onReturn,
-            ...(_handleEvent({event: "onCall", callee, args, handler: handlers.onCall}))
-          }).result
+          const onCallResult = _handleEvent({ callee, args, handler: onCall }) || {};
+          return _handleEvent({ callee, args, handler: onReturn, ...onCallResult }).result;
         } catch (caughtValue) {
-          return _handleCatch(handlers.onCatch, callee, args, caughtValue)
+          return _handleCatch({ handler: onCatch, callee, args, caughtValue });
         }
       }
     : async function (this: any, ...args: Parameters<F>) {
         const callee = ((...args: Parameters<F>) => c.apply(this, args)) as F
-        return Promise.resolve(_handleEvent({handler: handlers.onCall, event: "onCall", callee, args}).result)
-        .then(result => _handleEvent({handler: handlers.onReturn, event: "onReturn", callee, args, result}).result)
-        .catch(caughtValue => _handleCatch(handlers.onCatch, callee, args, caughtValue))
+        return Promise.resolve(_handleEvent({handler: onCall, callee, args}).result)
+        .then(result => _handleEvent({handler: onReturn, callee, args, result}).result)
+        .catch(caughtValue => _handleCatch({handler: onCatch, callee, args, caughtValue}))
       }
 
   Object.setPrototypeOf(wrapped, Object.getPrototypeOf(c))
