c552efe6e7e2832d14957acf8db2a26bc465fa04
Florian Patzke <florian.patzke@t-online.de>
2024-12-29 22:50:58 +0100

    Füge Wrapper-Funktionen für Ereignisbehandlung in withExecution hinzu; exportiere neue Funktionen für onCall, onCatch, onReturn und onError.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-import { withExecution } from './withExecution';
+import { withExecution } from "./withExecution";
+import { onCall, onCatch, onReturn } from "./types";
 
-export { withExecution };
+const withOnCall = <F extends (...args: any) => any>(
+    callee: F,
+    onCall: onCall<F>
+  ) => withExecution(callee, { onCall });
+
+const withOnCatch = <F extends (...args: any) => any>(
+  callee: F,
+  onCatch: onCatch<F>
+) => withExecution(callee, { onCatch });
+
+const withOnError = <F extends (...args: any) => any>(
+    callee: F,
+    onError: onCatch<F>
+    ) => withExecution(callee, { onCatch: (p) => p.caught instanceof Error ? ((p.event = "onError" as any),onError(p)) : p });
+
+const withOnReturn = <F extends (...args: any) => any>(
+  callee: F,
+  onReturn: onReturn<F>
+) => withExecution(callee, { onReturn });
+
+const withOnResult = <F extends (...args: any) => any>(
+  callee: F,
+  onResult: onReturn<F>
+) => withExecution(callee, { onReturn: (p) => p.result && onResult(p) });
+
+
+
+export { withExecution, withOnCall, withOnCatch, withOnReturn, withOnError, withOnResult };
