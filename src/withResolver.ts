dddf7a4f37888adc3dbae7530fd5d9aa8275d35c
Florian Patzke <fpatzke@ab-data.de>
2025-01-07 08:47:16 +0100

    Optimiere die Funktion resolve zur Verbesserung der Handler-Kette und der Fehlerbehandlung; passe die Logik zur Fehlerverarbeitung an und bereinige den Handler-Array.

--- src/withResolver.ts
+++ src/withResolver.ts
@@ -7,34 +7,26 @@ type HandlerChain = Handler[];
 
 function resolve(
   value: Array<any>,
-  handlerChain: HandlerChain = [] // Array of handler objects
+  handlerChain: HandlerChain = []
 ) {
   try {
-    while (handlerChain.length > 0) {
-      const [handlertype, handler] = handlerChain.pop()!;
-      if (handlertype === "catch") { continue }
-      value = (handler as any)(...value);
+   for (let i = 0; i < handlerChain.length; i++) {
+      if (handlerChain[i][0] === "catch") { continue }
+      value = (handlerChain[i][1] as any)(...value);
       if (value instanceof Promise) {
-        return handlerChain.reduce((acc, handler) =>  (acc as any)[handler[0]](handler[1]), value);
+        return handlerChain.reduce((acc, h) =>  (acc as any)[h[0]](h[1]), value);
       }
       value = [value];
     }
   } catch (error) {
-    //pop until next catch
-    while (handlerChain.length > 0) {
-      if (handlerChain[handlerChain.length-1][0] !== "catch") {
-        handlerChain.pop();
-      }
-    }
-
-    if (handlerChain.length === 0) {
-      throw error;
-    }
-
-    handlerChain[handlerChain.length][0] = "then";
-
+    const index = handlerChain.findIndex(([t]) => t === "catch");
+    if (index === -1) { throw error; }
+    handlerChain.splice(0, index);
+    handlerChain[0][0] = "then";
     return resolve([error], handlerChain);
   }
+
+  handlerChain.length = 0;
   return value[0];
 }
 
@@ -42,7 +34,6 @@ function withResolver<F extends (...args: any) => any>(callee: F) {
   const handlers: HandlerChain = [["then", callee]];
 
   function Resolver(this: any, ...args: Parameters<F>) {
-    handlers.reverse();
     return resolve(args, handlers);
   }
 
